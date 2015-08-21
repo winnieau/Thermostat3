00299ed3481128feb830be6d31ce1cbff16b9d37
Josh <Tattsy@hotmail.co.uk>
2015-08-21 12:25:33 +0100

    user can now make api requests for cities

--- JQapp.js
+++ JQapp.js
@@ -16,13 +16,17 @@ $(document).ready(function() {
   var weatherAPI = function (city) {
     $.ajax("http://api.openweathermap.org/data/2.5/weather?q="+city, {
       success: function (data) {
-        $('h2').html(data.main.temp);
+        $('h2').html( "The temperature in "+data.name+" is "+Math.round(data.main.temp-273.15)+"\u00B0C" );
       }
     });
   };
 
   value()
   weatherAPI("London")
+
+  $('#submit').click(function(){
+    weatherAPI($('#city').val())
+  });
 
   $('increasebutton').click(function() {
     thermostat.increaseBy(1);
